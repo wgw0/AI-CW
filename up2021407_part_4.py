1cbdc79ce31c137e2a0531df193ab16ec86daa7e
wgw0 <up2021407@myport.ac.uk>
2024-01-10 13:05:44 +0000

    changed function

--- up2021407_part_4.py
+++ up2021407_part_4.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-# This code does not work, it does not provide a correct output.
-
 # ReLU function for activation
 def relu(x):
     return np.maximum(0, x)
 
 # Derivative of the ReLU function for backpropagation
 def relu_derivative(x):
-    return np.where(x > 0, 1, 0)
+    return (x > 0).astype(float)
 
+# Define a Single Layer Neural Network class
 class SingleLayerNN(object):
     def __init__(self, input_size, output_size):
         # Initialise weights randomly with mean 0
@@ -26,10 +25,11 @@ class SingleLayerNN(object):
             self.synaptic_weights += np.dot(training_inputs.T, adjustments)
 
     def forward(self, inputs):
-        # Forward propagation through our network
+        # Forward propagation through the network
         return relu(np.dot(inputs, self.synaptic_weights))
 
 # Create training and target data
+# The training set consists of examples with known inputs and outputs.
 training_inputs = np.array([[0,0,1], [1,1,1], [1,0,1], [0,1,1]])
 training_outputs = np.array([[0,1,1,0]]).T
 
@@ -37,13 +37,16 @@ training_outputs = np.array([[0,1,1,0]]).T
 nn = SingleLayerNN(input_size=3, output_size=1)
 
 # Train the neural network
+# This process adjusts the weights of the network based on the training data.
 nn.train(training_inputs, training_outputs, 10000)
 
 # Test data
-test_inputs = np.array([[1,0,1], [0,0,0], [1,0,0]])
+# New data to test the performance of the neural network after training.
+test_inputs = np.array([[1,1,0], [0,0,0], [1,0,0]])
 test_outputs = nn.forward(test_inputs)
 
 # Display results
+# Print the test data and the corresponding predictions from the network.
 print("Test Inputs:")
 print(test_inputs)
 print("Test Outputs (Predictions):")
